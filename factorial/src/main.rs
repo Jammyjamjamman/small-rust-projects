773fe536de0eaba1361194eeaf4e3d98a4370688
Jammyjamjamman <jdsherratt3@gmail.com>
2021-05-02 04:51:21 +0100

    fix factorial code

--- factorial/src/main.rs
+++ factorial/src/main.rs
@@ -1,30 +1,12 @@
-trait Trait1 {
-}
-
-trait Trait2 {
-    fn trait2_func(&self) -> f32;
-}
-
-struct Test1;
-
-struct Test2<T> {
-    test1: T,
-}
-
-impl<T> Trait2 for Test2<T>
-    where T: Trait1 {
-    fn trait2_func(&self) -> f32 {
-        42.
+fn factorial(val: u64) -> u64 {
+    if val <= 1 {
+        1
     }
-
-}
-
-fn get_val<T: Trait2>(obj: T) -> f32 {
-    obj.trait2_func()
+    else {
+        val * factorial(val -1)
+    }
 }
 
 fn main() {
-    let test1_obj = Test1;
-    let test2_obj = Test2 { test1: test1_obj};
-    println!("{}", get_val(test2_obj))
+    println!("factorial 10 is {}", factorial(10));
 }
